f383f2e2c6a2d012d5434e0d1e97d5685971fa8d
wingram1 <waingram96@gmail.com>
2021-12-20 17:01:42 -0600

    added name variable and prompts

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,14 @@
-window.alert("This is an alert! JavaScript is running! :O");
+var playerName = window.prompt("What is your robot's name?");
+// what is this?
+console.log(playerName);
+window.alert("Hello, " + [playerName]);
+
+console.log("Our robot's name is " + playerName)
+console.log(10 + 10);
+
+//creates a function called "fight"
+function fight() {
+    window.alert("The fight has begun! >:D");
+}
+
+// fight();
